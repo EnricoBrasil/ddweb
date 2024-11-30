23c4e5d412fcbdff0811d5ba62e8247302b5cc71
enrico brasil <enrico.brasil@prit.app>
2024-11-30 18:36:48 -0300

    feature: links

--- src/pages/MenuPrincipal/index.jsx
+++ src/pages/MenuPrincipal/index.jsx
@@ -9,9 +9,21 @@ function MenuPrincipal() {
     <div className='logica'>
         <h1>Logica de programação</h1>
         <ul className='listaVideos'>
-            <li className='videos'>Video 1</li>
-            <li className='videos'>Video 2</li>
-            <li className='videos'>Video 3</li>
+            <li className='videos'><a href="https://www.youtube.com/watch?v=gMxQ8vxH9Vk">Lógica de programação, por onde começar ? (Guia RÁPIDO de estudos completo para INICIANTES)</a></li>
+            <li className='videos'><a href="https://www.youtube.com/watch?v=FF1wTboPbkc&t=2s">O que é Lógica de Programação | Duvido você não entender agora</a></li>
+            <li className='videos'><a href="https://www.youtube.com/watch?v=AhHPE_93nAk">Aprendendo Lógica de Programação</a></li>
+        </ul>
+        <h1>Front-end</h1>
+        <ul className='listaVideos'>
+            <li className='videos'><a href="https://www.youtube.com/watch?v=3oSIqIqzN3M">5 MINUTOS DE HTML PARA INICIAR EM PROGRAMAÇÃO!</a></li>
+            <li className='videos'><a href="https://www.youtube.com/watch?v=BKATrAAWrRo">CSS EM 15 MINUTOS! ESTILIZAÇÃO, POSITION E MAIS..</a></li>
+            <li className='videos'><a href="https://www.youtube.com/watch?v=WRlfwBof66s">Aprenda JAVASCRIPT em apenas 5 MINUTOS (2023)</a></li>
+        </ul>
+        <h1>Back-end</h1>
+        <ul className='listaVideos'>
+            <li className='videos'><a href="https://www.youtube.com/watch?v=bhXaHKtklV8">Programação Back-end</a></li>
+            <li className='videos'><a href="https://www.youtube.com/watch?v=LNhslFddZ-k">Guia de estudos - ̗̀ BACKEND ̖́-</a></li>
+            <li className='videos'><a href="https://www.youtube.com/watch?v=wXMlMsDvk2M">Como se tornar um desenvolvedor BACKEND? (Passo a passo / Roadmap)</a></li>
         </ul>
     </div>
   </div>
